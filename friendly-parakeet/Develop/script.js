4ea946c0dd6d6ecb4cd7e7f336267b638f6bdd35
Shabadoo9 <brinstonzbpe@gmail.com>
2023-06-01 03:27:03 -0400

    added if statements & password length limit

--- friendly-parakeet/Develop/script.js
+++ friendly-parakeet/Develop/script.js
@@ -1,14 +1,37 @@
 // Assignment code here
 function generatePassword() {
-  const length = parseInt(prompt("how many characters would you like your password to be?"));
-  
+  var passwordLength = parseInt(prompt("between 8 and 128 how many characters would you like your password to be?"));
+
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Please enter a valid number between 8 and 128.");
+    return "";
+  }
   const upperCase = confirm("Would you like to use uppercase letters?");
   const lowerCase = confirm("Would you like to use lowercase letters?");
   const numbers = confirm("would you like to use numbers?");
   const special = confirm("would you like to use special characters?");
+
+  let possiblechar ="";
+
+  if (upperCase) {
+    possibleChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  if (lowerCase) {
+    possibleChars += "abcdefghijklmnopqrstuvwxyz";
+  }
+
+  if (numbers) {
+    possibleChars += "0123456789";
+  }
+
+  if (special) {
+    possibleChars += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  }
+
+
+
 }
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -23,4 +46,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword, generatePassword);
+generateBtn.addEventListener("click", writePassword,);
